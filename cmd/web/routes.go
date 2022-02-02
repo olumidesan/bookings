ab0a4adbc9b886f484bc43824707073de7a050de
olumidesan <josiahakinremi@gmail.com>
2022-02-02 23:41:20 +0000

    Middleware test

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,15 +3,22 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"github.com/olumidesan/bookings/pkg/config"
 	"github.com/olumidesan/bookings/pkg/handlers"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	// Using chi router
+	mux := chi.NewRouter()
+
+	// Catch panics and stack-trace
+	mux.Use(middleware.Recoverer)
+	mux.Use(WriteToConsole)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 
 	return mux
 }
